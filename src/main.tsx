0ebe8ba5f14e48d8ead805e12e31214e30c1562a
lianxinbo <lianxinbo@hengli.com>
2023-04-17 11:24:10 +0800

    tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
+import './tailwind.css';
 import App from './App';
 
 const root = document.getElementById('root') as HTMLElement;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
