457eb50ceb355b2d3ec16eb599adac2eca1af243
lianxinbo <lianxinbo@hengli.com>
2023-07-11 16:54:15 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import { useRoutes } from 'react-router-dom';
-import Header from '@/router/Header';
-import RouteTable from '@/router/RouteTable';
 
 import './App.css';
 
 function App() {
-  const element = useRoutes(RouteTable);
-  return (
-    <div className='app'>
-      <Header />
-      {element}
-    </div>
-  );
+  return <div className='app'></div>;
 }
 
 export default App;
--- src/router/Header/Logo.tsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-  return (
-    <div className='logo'>
-      <img className='icon' src='/vite.svg' alt='' />
-      <span style={{ marginLeft: '20px' }}>React</span>
-    </div>
-  );
-}
